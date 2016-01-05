927cea12a676f530e172e1e9dd9e5bc3733a18aa
ilieash <ilieash.bulbul@gmail.com>
2016-01-05 14:38:53 +1100

    Update filebotamc.sh

--- files/bin/filebotamc.sh
+++ files/bin/filebotamc.sh
@@ -6,4 +6,4 @@ TORRENT_LABEL=$3
 
 /media/rt/bin/subliminal_script/Subliminal.py -S /media/rt/staging -k -L /media/rt/bin/subliminal_script/incronrun.log
 sleep 10
-filebot -script fn:amc --output "$HOME/media" --log-file amc.log --action move --conflict auto -non-strict --def clean=y unsorted=y pushbullet=dTYbfhCycFiG25lYOBtFNzbxeMKQRwSB  "ut_dir=$TORRENT_PATH" "ut_kind=multi" "ut_title=$TORRENT_NAME" "ut_label=$TORRENT_LABEL" &
+filebot -script fn:amc --output "$HOME/media" --log-file amc.log --action move --conflict auto -non-strict --def clean=y unsorted=y pushbullet=  "ut_dir=$TORRENT_PATH" "ut_kind=multi" "ut_title=$TORRENT_NAME" "ut_label=$TORRENT_LABEL" &
