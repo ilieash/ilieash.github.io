2dffe4cf17fa7790357de80f7a6753ea883ece32
ilieash <ilieash.bulbul@gmail.com>
2016-01-05 14:29:01 +1100

    Update backup.sh

--- files/backup_script/backup.sh
+++ files/backup_script/backup.sh
@@ -1,5 +1,5 @@
-export PASSPHRASE='BuL-?F3rx@Jx*3jF=wvjhLdSxKPYqk4Rxat$&yNhV+9^K5#2CEE%gGXKGksjY28wdK4#ww-YKEE-Wnd$VQTh+78GEmjLX5Tr4^*W'
-export FTP_PASSWORD='KillBill2015'
+export PASSPHRASE=''
+export FTP_PASSWORD=''
 duplicity /home ftp://sd-70029@dedibackup-dc3.online.net/home
 duplicity /etc ftp://sd-70029@dedibackup-dc3.online.net/etc
 duplicity --exclude /var/tmp --exclude /var/www/owncloud/data /var/www ftp://sd-70029@dedibackup-dc3.online.net/var/www
